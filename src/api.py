a73bd536e677e699fea49a717ccda01ce7955f18
Jhonatanslopes <jhonatans.ti@icloud.com>
2022-03-26 13:13:15 -0300

    create api to deploy

--- src/api.py
+++ src/api.py
@@ -5,14 +5,11 @@ import sklearn
 import numpy as np
 from flask import Flask, request, Response
 from lightgbm import LGBMClassifier
-from class_.FraudDetection import FraudDetection
+from class_.FraudDetection import FraudDetection 
 
 
-# loading model
-model = pickle.load(open('C:/Users/Jhonatans/projects/ML/Classification/Financial-Fraud-Detection/src/model/lgbm.pkl', 'rb'))
-
-# initialize API
-app = Flask(__name__)
+model = pickle.load(open('model/lgbm.pkl', 'rb')) # loading model
+app = Flask(__name__) # initialize API
 
 @app.route('/fraudDetection/predict', methods=['POST'])
 def fraudDetection_predict():
@@ -58,5 +55,25 @@ def fraudDetection_predict():
     
     
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='127.0.0.1', port=port, debug=True)
+    porta = os.environ.get('PORT', 5000)
+    app.run(host='0.0.0.0', port=porta)
+
+
+"""
+import json
+import requests
+
+# data to json
+data = json.dumps(x_test.to_dict(orient='records'))
+
+#url = 'http://127.0.0.1:5000/fraudDetection/predict'
+url = 'https://api-fraud.herokuapp.com/fraudDetection/predict' # local host
+header = {'content-type': 'application/json'} # set type as json
+
+# request with method POST
+response = requests.post(url, data=data, headers=header)
+print('Status code: {}'.format(response.status_code))
+
+# json to dataframe
+d1 = pd.DataFrame(response.json(), columns=response.json()[0].keys())
+d1"""
